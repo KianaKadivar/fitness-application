211005cd9ce8a83d2c54aa2ae18939e0affe53d3
kiana.kad <kianakadivar05@gmail.com>
2025-12-17 21:47:22 +0330

    feat(navbar): add action button to navbar
    - Created reusable ActionButton component for primary call to action.
    - Integrated ActionButton into the Navbar layout.

--- frontend/src/Scenes/Navbar/index.tsx
+++ frontend/src/Scenes/Navbar/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Bars3Icon,XMarkIcon } from '@heroicons/react/16/solid'
 import Logo from "@/assets/Logo.png"
 import Link from './Link'
 import type { SelectedPage } from '../../Shared/types'
+import useMediaQuery from '../../Hooks/useMediaQuery'
+import ActionButton from '../../Shared/ActionButton'
 
 type Props = {
     selectedPage:SelectedPage,
@@ -11,6 +13,8 @@ type Props = {
 
 const Navbar = ({selectedPage,setSelectedPage}: Props) => {
   const flexBetween="flex items-center justify-between"
+  const isAboveMediumScreens=useMediaQuery("(min-width:1060px)")
+  const [IsMenuToggled,setIsMenuToggled]=useState<boolean>(false)
   return (
     <nav >
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -19,6 +23,7 @@ const Navbar = ({selectedPage,setSelectedPage}: Props) => {
             {/* Left side */}
             <img src={Logo} alt="Logo" />
             {/* Right side */}
+            {isAboveMediumScreens?(
             <div className={`${flexBetween} w-full`}>
               <div className={`${flexBetween} gap-8 text-sm`} >
                   <Link page='Home' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
@@ -28,9 +33,9 @@ const Navbar = ({selectedPage,setSelectedPage}: Props) => {
                   </div>
                   <div className={`${flexBetween} gap-8`}>
                     <p>Sign In</p>
-                    <button>Become a Member</button>
+                    <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
                   </div>
-            </div>
+            </div>):(<button className='rounded-full bg-secondary-500 p-2' onClick={()=>{setIsMenuToggled(!IsMenuToggled)}}><Bars3Icon className='h-6 w-6 text-white'/></button>)}
           </div>
         </div>
 
