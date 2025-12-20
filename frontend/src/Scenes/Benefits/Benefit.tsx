3cee843e92a46e998c28231a492df5aaa233693d
kiana.kad <kianakadivar05@gmail.com>
2025-12-20 16:59:23 +0330

    feat(benefits): add Benefit animated cards
    - Implemented Benefits section with structured data and reusable Benefit cards.
    - Added Framer Motion animations with staggered child transitions.

--- frontend/src/Scenes/Benefits/Benefit.tsx
+++ frontend/src/Scenes/Benefits/Benefit.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { type JSX } from 'react'
+import { SelectedPage } from '../../Shared/types'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import {motion} from "framer-motion"
 
-type Props = {}
+const childVariant={
+    hidden:{opacity:0,scale:0.9},
+    visible:{opacity:1,scale:1}
+}
 
-const Benefit = (props: Props) => {
+type Props = {
+    icon:JSX.Element,
+    title:string,
+    description:string,
+    setSelectedPage:(value:SelectedPage)=>void
+}
+
+const Benefit = ({icon,title,description,setSelectedPage}: Props) => {
   return (
-    <div className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'>
-
-    </div>
+    <motion.div className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'
+        variants={childVariant}
+        >
+        <div className='mb-4 flex justify-center'>
+            <div className='rounded-full border-2 border-gray-100 bg-primary-100 p-4'>
+                {icon}
+            </div>
+        </div>
+        <h4 className='font-bold'>{title}</h4>
+        <p className='my-3'>{description}</p>
+        <AnchorLink className='font-bold text-sm underline text-primary-500 hover:text-secondary-500'
+            onClick={()=>setSelectedPage(SelectedPage.ContactUs)}
+            href={`#${SelectedPage.ContactUs}`}
+            >
+                <p>Learn More</p>
+        </AnchorLink>
+    </motion.div>
   )
 }
 
--- frontend/src/Scenes/Benefits/index.tsx
+++ frontend/src/Scenes/Benefits/index.tsx
@@ -27,31 +27,52 @@ const benefits:Array<BenefitType>=[
     }
 ]
 
+const container={
+    hidden:{},
+    visible:{
+        transition:{staggerChildren:0.2}
+    }
+}
+
 const Benefits = ({setSelectedPage}: Props) => {
   return (
     <section id='benefits' className='mx-auto my-0 md:my-25 min-h-full w-5/6 py-20'>
         <motion.div onViewportEnter={()=>setSelectedPage(SelectedPage.Benefits)}>
             {/* Header */}
-            <div className='md:my-5 md:w-3/5'>
+            <motion.div className='md:my-5 md:w-3/5' 
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once:true,amount:0.5}}
+                transition={{delay:0.2,duration:0.5}}
+                variants={{
+                    hidden:{opacity:0,x:-50},
+                    visible:{opacity:1,x:0}
+                }} 
+                >
                 <HText>MORE THAN JUSY GYM.</HText>
                 <p className='my-5 text-sm'> 
                     We provide world class fitness equipment, trainers and classes to
                     get you to your ultimate fitness goals with ease. We provide true
                     care into each and every member.
                 </p>
-            </div>
+            </motion.div>
             {/* Benefits */}
-            <div className='mt-5 items-center justify-between gap-8 md:flex'>
+            <motion.div className='mt-5 items-center justify-between gap-8 md:flex' 
+                initial="hidden"
+                whileInView={"visible"}
+                viewport={{once:true,amount:0.5}}
+                variants={container}
+            >
                 {benefits.map((benefit)=>(
                     <Benefit
                     key={benefit.title}
                     icon={benefit.icon}
                     title={benefit.title}
                     description={benefit.description}
-                    setSelectedPag={setSelectedPage}
+                    setSelectedPage={setSelectedPage}
                     />
                 ))}
-            </div>
+            </motion.div>
         </motion.div>
     </section>
   )
